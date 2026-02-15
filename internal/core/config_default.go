0ee80937270b691ef27e04ee5de37197d1f23cac
kjkrol <kjkrol@gmail.com>
2026-02-15 23:15:24 +0100

    set 96KB as L1 szie for non ARM64 CPUs

--- internal/core/config_default.go
+++ internal/core/config_default.go
@@ -2,7 +2,4 @@
 
 package core
 
-// DefaultChunkSize: 32KB for x86/AMD64 and others.
-// Standard x86 processors usually have smaller L1 Data Cache (32KB - 48KB).
-// 32KB is a safe bet to leave room for stack and other data.
-const L1DataCacheSize = 16 * 1024
+const L1DataCacheSize = 96 * 1024 // KB
